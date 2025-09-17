58f49cc74d3f554dd95b67aec9a1212a2682676c
Martin Levesque <martin.levesque@epita.fr>
2025-09-17 19:12:32 +0200

    feat(setup.sh): setup routine to compile module

---
+++ setup.sh
@@ -0,0 +1,11 @@
+cd gistre26-dril-sdk/dril-sdk/
+echo "Export ENV variables"
+export PATH=$(pwd)/buildroot-dril-sdk/aarch64-buildroot-linux-gnu_sdk-buildroot/bin:"$PATH"
+export KDIR=$(pwd)/buildroot-dril-sdk/linux-build
+export KBUILD_EXTRA_SYMBOLS=$(pwd)/mfrc522_emu-sdk/Module.symvers
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-buildroot-linux-gnu-
+cd -
+cd groupe7/src/
+echo "Compiling the module"
+make modules
